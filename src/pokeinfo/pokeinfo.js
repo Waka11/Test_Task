cf0bc4a67a51db1e74a5960cea30c06f997c652e
Andy Pavliuk <andypavliuk11@gmail.com>
2022-11-10 20:02:58 +0200

    Create pokeinfo.js

---
+++ src/pokeinfo/pokeinfo.js
@@ -0,0 +1,26 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+import '../App.css';
+
+function PokeInfo() {
+    return (
+    <div className='pokemon_info'>
+        <div className="card">
+        <img className="card-img-top" src="..." alt="" />
+            <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+            </div>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">Cras justo odio</li>
+          <li className="list-group-item">Dapibus ac facilisis in</li>
+          <li className="list-group-item">Vestibulum at eros</li>
+        </ul>
+        <div className="card-body">
+        </div>
+    </div>
+  </div>
+
+    );
+}
+
+export default PokeInfo;
